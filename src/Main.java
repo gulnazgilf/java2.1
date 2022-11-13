9de6111e5157286b00158d988f7d9e34f0d361bc
Гульназ Гильфанова <gulnazgilf21@gmail.com>
2022-11-13 18:22:51 +0300

    Added src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-
+        int moneyintheAccount = 50;
+        int topUp = 1500;
+        int bonus = topUp / 100;
+        if (topUp > 1000) {
+            System.out.println("бонус начисляется");
+        } else {
+            System.out.println("bonus Неначисляется");
+        }
+        System.out.println(moneyintheAccount + topUp + bonus);
     }
 }
